5bd93364fdd0f5aa06af6c8cb27889e316195188
d-najd <59766732+d-najd@users.noreply.github.com>
2026-06-16 17:59:38 +0200

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.ts
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums: number[], target: number): number {
+        let low = 0;
+        let high = nums.length - 1;
+
+        while (low <= high) {
+            const mid = Math.floor((low + high) / 2);
+
+            if (nums[mid] === target) {
+      return mid; // Target found
+    } else if (nums[mid] < target) {
+      low = mid + 1; // Discard the left half
+    } else {
+      high = mid - 1; // Discard the right half
+    }
+  }
+  return -1; // Target not found    }
+}
+}
