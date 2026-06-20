5afd8174492954075a37f2ede17160e5a967128f
d-najd <59766732+d-najd@users.noreply.github.com>
2026-06-20 22:39:42 +0200

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.ts
@@ -0,0 +1,59 @@
+class PrefixTree {
+    char: string
+    isEnd: boolean
+    entries: PrefixTree[] = []
+
+    constructor() {
+
+    }
+
+    /**
+     * @param {string} word
+     * @return {void}
+     */
+    insert(word: string): void {
+        if (word.length === 0) return
+
+        const entry = this.entries.find(o => o.char === word[0])
+        if (entry) {
+            if (word.length === 1) {
+                entry.isEnd = true
+            }
+            entry.insert(word.slice(1, word.length))
+        } else {
+            const newEntry = new PrefixTree()
+            newEntry.char = word[0]
+            newEntry.isEnd = word.length === 1
+            this.entries.push(newEntry)
+            newEntry.insert(word.slice(1))
+        }
+    }
+
+    /**
+     * @param {string} word
+     * @return {boolean}
+     */
+    search(word: string): boolean {
+        if (word.length === 0 && this.isEnd) return true
+
+        const entry = this.entries.find(o => o.char === word[0])
+        if (!entry) { 
+            return false
+        }
+
+        return entry.search(word.slice(1))
+    }
+
+    /**
+     * @param {string} prefix
+     * @return {boolean}
+     */
+    startsWith(prefix: string): boolean {
+        if (prefix.length === 0) return true
+
+        const entry = this.entries.find(o => o.char === prefix[0])
+        if (!entry) return false
+        
+        return entry.startsWith(prefix.slice(1))
+    }
+}
