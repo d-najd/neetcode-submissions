f8a83a1704fc6914af504b58da5edf9c2c80244f
d-najd <59766732+d-najd@users.noreply.github.com>
2026-06-28 17:42:18 +0200

    Add: serialize-and-deserialize-binary-tree - submission-1

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-1.py
@@ -0,0 +1,73 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Codec:
+    
+    # Encodes a tree to a single string.
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        if root == None:
+            return ""
+        
+        result = []
+        result.append(root.val)
+        self.serializeRecurse(root, result)
+
+        parsed = ""
+        for cur in result:
+            
+            if cur == None:
+                parsed += ":"
+                continue
+            parsed += f"{cur}:"
+        return parsed
+
+    def serializeRecurse(self, cur: Optional[TreeNode], result):
+        if cur != None:
+            if cur.left != None:
+                result.append(cur.left.val)
+            else:
+                result.append(None)
+            if cur.right != None:
+                result.append(cur.right.val)
+            else:
+                result.append(None)
+            self.serializeRecurse(cur.left, result)
+            self.serializeRecurse(cur.right, result)
+    
+    counter = 1
+    # Decodes your encoded data to tree.
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        if "":
+            return None
+        
+        test = data.split(":")
+        result = TreeNode(test[0])
+        self.deserializeBuilder(result, test)
+
+        return result
+    
+    def deserializeBuilder(self, cur, test):
+        if self.counter + 1 >= len(test):
+            return 
+
+        if cur != None:
+            lv = test[self.counter]
+            rv = test[self.counter+1]
+            if lv != '':
+                lv = TreeNode(lv)
+            else:
+                lv = None
+            if rv != '':
+                rv = TreeNode(rv)
+            else:
+                rv = None
+
+            cur.left = lv
+            cur.right = rv
+            self.counter += 2
+            self.deserializeBuilder(cur.left, test)
+            self.deserializeBuilder(cur.right, test)
