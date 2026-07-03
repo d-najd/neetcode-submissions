a5dde0b8a8ca7524e04519911cfbb69280e64129
d-najd <59766732+d-najd@users.noreply.github.com>
2026-07-03 20:07:50 +0200

    Add: valid-tree - submission-9

---
+++ Data Structures & Algorithms/valid-tree/submission-9.py
@@ -0,0 +1,39 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        nodes = [i for i in range(n)]
+        children = [1 for i in range(n)]
+
+        def find(n1):
+            cur = n1
+
+            while nodes[cur] != cur:
+                cur = find(nodes[cur])
+
+            return cur
+        def union(n1, n2):
+            n1p = find(n1)
+            n2p = find(n2)
+
+            if n1p == n2p:
+                return False
+            
+            if (children[n2p] > children[n1p]):
+                nodes[n2p] = n1p
+                children[n2p] += 1
+            else:
+                nodes[n1p] = n2p
+                children[n1p] += 1
+
+        for n1, n2 in edges:
+            if (union(n1, n2) == False):
+                return False
+        
+        rootFound = False
+        for child in children:
+            if child == 1:
+                if rootFound:
+                    return False
+                else:
+                    rootFound = True
+            
+        return True
